b4b3dc4ecfd0cb2d0c87ece00a8eff2b08a36faf
Vivekanand Rao <vivekanand.rao@gmail.com>
2018-08-30 21:53:18 -0700

    Modal, Scrollspy, Menu Animation, Keyframe animation, Animating with Scrollspy.

---
+++ bootstrap-layouts/builds/js/script.js
@@ -0,0 +1,41 @@
+$(function(){
+
+  var topOffset = 55;
+
+  $('.navbar a[href*=\\#]:not([href=\\#])').click(
+    function(){
+      if(location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname){
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if(target.length){
+          $('html,body').animate(
+            {
+              scrollTop: target.offset().top - topOffset + 2
+            },
+            500
+          );
+          return false;
+        }// target.length
+      }
+    }
+  ); // smooth scrolling 
+
+
+  $(window).on('activate.bs.scrollspy', function(){
+    var hash = $('.site-nav').find('a.active').attr('href');
+    if(hash !== '#page-hero'){
+      $('header nav').addClass('inbody');
+    }else{
+      $('header nav').removeClass('inbody');
+    }
+
+    $('#page-media .layout-animation').css('visibility: hidden');
+    if(hash === '#page-media'){
+      $('#page-media .layout-animation').addClass('animated fadeInRight');
+    }
+  });
+
+  $('#site-modal').on('show.bs.modal', function(event){
+    $(this).find('.modal-content img').attr('src', $(event.relatedTarget).data('highres'))
+  });
+});
